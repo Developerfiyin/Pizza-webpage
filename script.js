f62e49c77decea98001dba69d25bf5eaeb08ef46
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-07-21 23:44:07 +0100

    updated js file

---
+++ script.js
@@ -0,0 +1,12 @@
+/*===========SHOW MENU ======================*/
+
+const navMenu = document.getElementById("nav-menu");
+navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
+
+/* ===============MENU SHOw=========== */
+if (navToggle) {
+  navToggle.addEventListenter("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
