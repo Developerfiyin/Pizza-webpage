dc1b0b03e839378cd335dc7188659a35001dc154
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-07-24 21:49:01 +0100

    added script to menu mobile

--- script.js
+++ script.js
@@ -18,7 +18,24 @@ if (navClose) {
   });
 }
 
+//======REMOVE MENU MOBILE
+const navLink = document.querySelectorAll("nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  //When we click on each nav-link, we remove the show-menu close
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /* ===========PEKA*/
 document.getElementById("btn").onclick = function () {
   alert("Peka is 16 years old right now!");
 };
+
+/*  TODAY I LEARNT IF AND ELSE STATEMENT */
+
+var iLoveJs = true;
+if (iLoveJs) {
+  console.log("Good girl Keep it going ");
+} else console.log("You dont have a choice, Go back to it");
